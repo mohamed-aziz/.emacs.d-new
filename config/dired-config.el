0fc2c2f7461d0d336829f535faee7cf84a3b5de5
mohamed-aziz <medazizknani@gmail.com>
2017-08-20 12:46:27 +0100

    Add download file function (blocking)

--- config/dired-config.el
+++ config/dired-config.el
@@ -3,4 +3,28 @@
 
 (define-key dired-mode-map "b" 'emms-play-dired)
 
+
+;; from https://stackoverflow.com/questions/4448055/download-a-file-with-emacs-lisp
+(require 'url)
+
+(defun download-file (&optional url download-dir download-name)
+  (interactive)
+  (let ((url (or url
+                 (read-string "Enter download URL: "))))
+    (let ((download-buffer (url-retrieve-synchronously url)))
+      (save-excursion
+        (set-buffer download-buffer)
+        ;; we may have to trim the http response
+        (goto-char (point-min))
+        (re-search-forward "^$" nil 'move)
+        (forward-char)
+        (delete-region (point-min) (point))
+        (write-file (concat (or default-directory
+				download-dir
+				"~/Downloads"
+                            (or download-name
+                                (car (last (split-string url "/" t))))))))))
+
+
+
 (provide 'dired-config)
