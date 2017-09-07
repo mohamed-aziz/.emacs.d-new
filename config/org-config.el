f2d2bc221eca31f59d9cf94c47fc576d6e86e959
mohamed-aziz <medazizknani@gmail.com>
2017-09-07 11:23:46 +0100

    Make writing in org files more pleasant

--- config/org-config.el
+++ config/org-config.el
@@ -2,5 +2,9 @@
 
 (setq org-log-done 'time)
 
+(add-hook 'org-mode-hook '(lambda ()
+			    (setq left-margin-width 5)
+			    (setq right-margin-width 5)
+			    (visual-line-mode)))
 
 (provide 'org-config)
