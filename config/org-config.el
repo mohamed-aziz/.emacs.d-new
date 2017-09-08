25c61b91b7bc7e566db20271ed8ac6d688512685
mohamed-aziz <medazizknani@gmail.com>
2017-09-08 21:07:52 +0100

    Add org-agenda stuff

--- config/org-config.el
+++ config/org-config.el
@@ -7,4 +7,14 @@
 			    (setq right-margin-width 5)
 			    (visual-line-mode)))
 
+;; set the agenda files to my movies and mylife
+
+(setq org-agenda-files (list
+			(expand-file-name "~/movie.org")
+			(expand-file-name "~/mylife.org")))
+
+;;(define-key org-mode-map (kbd "C-c a") 'org-agenda)
+
+(global-set-key (kbd "C-c a") 'org-agenda)
+
 (provide 'org-config)
