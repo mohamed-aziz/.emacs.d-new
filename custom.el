a2ee312170391cf6c1d5258fc781bacf7f946366
mohamed-aziz <medazizknani@gmail.com>
2017-10-08 11:32:18 +0100

    Update.

--- custom.el
+++ custom.el
@@ -5,14 +5,27 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("0e0c37ee89f0213ce31205e9ae8bce1f93c9bcd81b1bcda0233061bb02c357a8" default)))
+    ("086970da368bb95e42fd4ddac3149e84ce5f165e90dfc6ce6baceae30cf581ef" "0e0c37ee89f0213ce31205e9ae8bce1f93c9bcd81b1bcda0233061bb02c357a8" default)))
+ '(org-html-html5-fancy t)
  '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello))
  '(package-selected-packages
    (quote
-    (noflet zpresent zenburn-theme yoshi-theme xterm-color xclip which-key web-mode web-beautify w3m undo-tree transmission tao-theme ssass-mode speed-type sourcerer-theme solarized-theme smooth-scrolling smex resize-window projectile prodigy pomodoro plan9-theme php-mode perspective paredit origami org-trello olivetti nginx-mode multi-term monochrome-theme mmm-mode minimal-theme markdown-mode magithub lua-mode js2-mode inkpot-theme image+ ido-vertical-mode highlight-parentheses gruvbox-theme ggtags flycheck flx-ido fill-column-indicator expand-region emms-player-mpv emmet-mode elpy edit-indirect dired-du diminish cyberpunk-theme color-theme ace-window ace-jump-mode))))
+    (symon noflet zpresent zenburn-theme yoshi-theme xterm-color xclip which-key web-mode web-beautify w3m undo-tree transmission tao-theme ssass-mode speed-type sourcerer-theme solarized-theme smooth-scrolling smex resize-window projectile prodigy pomodoro plan9-theme php-mode perspective paredit origami org-trello olivetti nginx-mode multi-term monochrome-theme mmm-mode minimal-theme markdown-mode magithub lua-mode js2-mode inkpot-theme image+ ido-vertical-mode highlight-parentheses gruvbox-theme ggtags flycheck flx-ido fill-column-indicator expand-region emms-player-mpv emmet-mode elpy edit-indirect dired-du diminish cyberpunk-theme color-theme ace-window ace-jump-mode)))
+ '(safe-local-variable-values
+   (quote
+    ((eval progn
+	   (defun set-test-project nil "docstring"
+		  (elpy-test-pytest-runner
+		   (concat
+		    (file-name-as-directory
+		     (elpy-project-find-projectile-root))
+		    "backend/tests")
+		   nil nil nil))
+	   (setq projectile-project-test-cmd
+		 (function set-test-project)))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#171717" :foreground "#DADADA" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 114 :width normal :family "DejaVu Sans Mono")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 111 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
--- init.el
+++ init.el
@@ -1,11 +1,4 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
 (require 'org)
 (org-babel-tangle-file (concat user-emacs-directory "Knani.org"))
-(load-file (concat user-emacs-directory "Knani.el"))
-(byte-compile-file (concat user-emacs-directory "Knani.el"))
+(load-file (concat user-emacs-directory "init.el"))
+(byte-compile-file (concat user-emacs-directory "init.el"))
